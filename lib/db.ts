2ec3b52bd272afdb04c718aab2c902cadaf4a13f
youngvform <austinhbk@naver.com>
2022-04-24 23:27:08 +0900

    initial setup for firebase auth github, firestore, chakra-ui

---
+++ lib/db.ts
@@ -0,0 +1,25 @@
+import app from './firebase'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  setDoc,
+  doc
+} from 'firebase/firestore'
+import { UserState } from './auth'
+
+const db = getFirestore(app)
+
+export async function createUser(uid: string, data: UserState) {
+  try {
+    await setDoc(
+      doc(db, 'users', uid),
+      {
+        ...data
+      },
+      { merge: true }
+    )
+  } catch (error) {
+    console.error(`Error: adding document `, error)
+  }
+}
