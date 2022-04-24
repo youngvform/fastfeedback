cf7c6f61dceb34c194ab0bf842a4bba41fb76738
youngvform <austinhbk@naver.com>
2022-04-24 16:41:52 +0900

    Change description

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <h1 className={styles.title}>Fast Feedback</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          I'm exiting to editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
