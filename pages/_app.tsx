46692b4235eb7b563baa3d459e34eb5c281c6ec5
youngvform <austinhbk@naver.com>
2022-04-25 00:02:13 +0900

    set path alias and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,38 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import { AuthProvider } from '../lib/auth'
-import theme from '../styles/theme'
+import { AuthProvider } from '@/lib/auth'
+import theme from '@/styles/theme'
+import Head from 'next/head'
+import { css, Global } from '@emotion/react'
+
+const GlobalStyle = () => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme} resetCSS>
+    <ChakraProvider theme={theme}>
       <AuthProvider>
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
